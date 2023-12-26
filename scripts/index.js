450fb4394c5308d6e899f01c55281ce8a72737f6
Lucy Antonova <ptashka6060@gmail.com>
2023-12-26 19:45:15 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+//Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+//DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+const createCard = (data, deleteCallback) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector(".card__title").textContent = data.name;
+  cardElement.querySelector(".card__delete-button");
+  cardElement.addEventListener("click", deleteCallback);
+
+  return cardElement;
+};
+
+//Функция удаления карточки
+const deleteCard = (event) => {
+  event.target.closest(".card").remove();
+};
+
+//Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  cardsContainer.append(createCard(cardData, deleteCard));
+});
